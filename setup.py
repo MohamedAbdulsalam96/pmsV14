22155ab5d1f44feba1e88ce81fedeb2ecd9b1e01
TridotsTech <54178464+TridotsTech@users.noreply.github.com>
2023-06-22 18:14:47 +0530

    Updated for V14

--- setup.py
+++ setup.py
@@ -1,65 +1,20 @@
-from __future__ import unicode_literals
-
-# imports - standard imports
-import os, shutil
-from distutils.command.clean import clean as Clean
-
-# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import re, ast
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in pms/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+from pms import __version__ as version
 
-
-with open('requirements.txt') as f:
-        install_requires = f.read().strip().split('\n')
-
-with open('pms/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-		
-class CleanCommand(Clean):
-        def run(self):
-                Clean.run(self)
-
-                basedir = os.path.abspath(os.path.dirname(__file__))
-
-                for relpath in ['build', '.cache', '.coverage', 'dist', 'frappe.egg-info']:
-                        abspath = os.path.join(basedir, relpath)
-                        if os.path.exists(abspath):
-                                if os.path.isfile(abspath):
-                                        os.remove(abspath)
-                                else:
-                                        shutil.rmtree(abspath)
-
-                for dirpath, dirnames, filenames in os.walk(basedir):
-                        for filename in filenames:
-                                _, extension = os.path.splitext(filename)
-                                if extension in ['.pyc']:
-                                        abspath = os.path.join(dirpath, filename)
-                                        os.remove(abspath)
-                        for dirname in dirnames:
-                                if dirname in ['__pycache__']:
-                                        abspath = os.path.join(dirpath,  dirname)
-                                        shutil.rmtree(abspath)
 
 setup(
-	name='pms',
+	name="pms",
 	version=version,
-	description='App for managing projects ',
-	author='Valiant Systems ',
-	author_email='vigneshwaran@valiantsystems.com',
+	description="App for managing projects ",
+	author="Tridots Team",
+	author_email="info@tridotstech.com",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires,
-	dependency_links=[
-                'https://github.com/frappe/python-pdfkit.git#egg=pdfkit'
-        ],
-        cmdclass = \
-        {
-                'clean': CleanCommand
-        }
-
+	install_requires=install_requires
 )
